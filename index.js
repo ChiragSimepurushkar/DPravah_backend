8595fae22d413c2c9584aabe11d8b034bee973f8
Chirag Nikant Simepurushkar <simepurushkarchirag84@gmail.com>
2025-08-04 00:22:43 +0530

    added deployed path

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"], // Allow both frontend apps
+    origin: ["https://d-pravah-frontend.vercel.app", "https://d-pravah-dashboard.vercel.app"], // Allow both frontend apps
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
