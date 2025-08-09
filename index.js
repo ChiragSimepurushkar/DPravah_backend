442b14c7fe512b23bcd48bff5a265c4e52deeb8a
Chirag Nikant Simepurushkar <simepurushkarchirag84@gmail.com>
2025-08-09 21:26:49 +0530

    authen again

--- index.js
+++ index.js
@@ -355,6 +355,22 @@ app.post('/login', async (req, res, next) => {
 //     //     }
 //     // })
 
+app.post('/verify',(req, res) => {
+  const token = req.cookies.token
+  if (!token) {
+    return res.json({ status: false })
+  }
+  jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
+    if (err) {
+     return res.json({ status: false })
+    } else {
+      const user = await User.findById(data.id)
+      if (user) return res.json({ status: true, user: user.username })
+      else return res.json({ status: false })
+    }
+  })
+});
+
 //     const authHeader = req.headers.authorization;
 
 //     if (!authHeader || !authHeader.startsWith('Bearer ')) {
